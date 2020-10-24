791159e0c3e46eca9bf456794cc5190192deb293
@nilesh_gopale <66791178+Nilesh1206@users.noreply.github.com>
2020-10-24 12:35:38 +0530

    Add files via upload

---
+++ widgets/appbar_title.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppBarTitleSubtitle extends StatelessWidget {
+  const AppBarTitleSubtitle({
+    Key key,
+    @required this.title,
+    @required this.subtitle,
+  });
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(title),
+        Text(
+          subtitle,
+          style:
+              // ignore: deprecated_member_use
+              Theme.of(context).textTheme.subhead.copyWith(color: Colors.white),
+        )
+      ],
+    );
+  }
+}
